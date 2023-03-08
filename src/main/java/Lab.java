d444a6ee3a1d62cd3707a21fc53122a861bf3792
jakob baldwin <jhbaldwin@pipeline.sbcc.edu>
2023-03-08 02:53:04 +0000

    Completed Comparisons Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -24,7 +24,13 @@ public class Lab {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        if (a==b) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
     }
 
     /**
@@ -33,6 +39,10 @@ public class Lab {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
+        if (a>b) {
+            return true;
+        }
+
         return false;
     }
     /**
@@ -41,7 +51,10 @@ public class Lab {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        if (a<b) {
+            return true;
+        }
+                return false;
     }
     /**
      * @param a the first number to be compared.
@@ -49,6 +62,9 @@ public class Lab {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
+        if (a>=b) {
+            return true;
+        }
         return false;
     }
     /**
@@ -57,6 +73,9 @@ public class Lab {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
+        if (a<=b) {
+            return true;
+        }
         return false;
     }
 
@@ -66,6 +85,9 @@ public class Lab {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
+        if (a!=b) {
+            return true;
+        }
         return false;
     }
 }
